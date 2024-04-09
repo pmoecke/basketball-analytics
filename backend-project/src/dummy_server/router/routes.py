0976356383ab5199e26135c483e0b45ceeefc4ef
fredriknystrom <fredriknystroms@gmail.com>
2024-04-09 09:56:14 +0200

    rm example project files

--- backend-project/src/dummy_server/router/routes.py
+++ backend-project/src/dummy_server/router/routes.py
@@ -6,9 +6,6 @@ API = "/api/"  # optional string
 
 def add_routes(app):
     api = Api(app)
-
-    api.add_resource(res.app_info.Environment, API + "app_info")
-    api.add_resource(res.scatter_data.DatasetResource, API + "data/<string:name>")
     api.add_resource(res.players.Players, API + "players/")
 
     return api
