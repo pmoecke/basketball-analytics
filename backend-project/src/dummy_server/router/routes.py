1f871d27e1de8bd92b014274ef9087c8d8a73fe7
fredriknystrom <fredriknystroms@gmail.com>
2024-06-04 22:09:12 +0200

    rm old api endpoints and routes

--- backend-project/src/dummy_server/router/routes.py
+++ backend-project/src/dummy_server/router/routes.py
@@ -6,13 +6,9 @@ API = "/api/"  # optional string
 
 def add_routes(app):
     api = Api(app)
-    api.add_resource(res.stats_old.StatsOld, API + "stats-old/")
     api.add_resource(res.overview_stats.OverviewStats, API + "overview-stats/")
     api.add_resource(res.stats.Stats, API + "stats/")
     api.add_resource(res.players.Players, API + "players/")
-    api.add_resource(res.leagues.Leagues, API + "leagues/")
-    api.add_resource(res.teams.Teams, API + "teams/")
     api.add_resource(res.projection.Projection, API + "projection/")
-    api.add_resource(res.scores.Scores, API + "scores/")
 
     return api
