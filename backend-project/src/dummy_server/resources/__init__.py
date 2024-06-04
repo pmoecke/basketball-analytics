413ab2a51d28f721c386e50d7b3e0e84c4075496
fredriknystrom <fredriknystroms@gmail.com>
2024-06-04 22:16:53 +0200

    fixed failing imports

--- backend-project/src/dummy_server/resources/__init__.py
+++ backend-project/src/dummy_server/resources/__init__.py
@@ -1,19 +1,11 @@
 from . import players
-from . import leagues
-from . import teams
-from . import stats_old
 from . import stats
 from . import overview_stats
 from . import projection
-from . import scores
 
 __all__ = [
     "players"
-    "leagues"
-    "teams"
-    "stats_old"
     "stats"
     "overview_stats"
     "projection"
-    "scores"
 ]
