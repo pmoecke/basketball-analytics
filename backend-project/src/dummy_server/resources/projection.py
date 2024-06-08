6d4b630faf559f1de15cba68f86f20ac8154567e
Yanik Künzi <ykuenzi@ethz.ch>
2024-06-08 12:42:16 +0200

    Finished projections based on individual columns and players

--- backend-project/src/dummy_server/resources/projection.py
+++ backend-project/src/dummy_server/resources/projection.py
@@ -33,6 +33,7 @@ class Projection(Resource):
         con = sqlite3.connect(os.path.join(os.environ["DATA_PATH"],
                                            "Players.db"))
         cur = con.cursor()
+        # In case we're provided with player_ids from the frontend
         if "player_id" in args and -1 not in args["player_id"]:
             cur.execute(f"SELECT name FROM Player WHERE player_id in ({
                 ", ".join(["?" for _ in range(len(args["player_id"]))])});",
@@ -40,24 +41,26 @@ class Projection(Resource):
 
             names = [name for name, *_ in cur.fetchall()]
             df = df[df["player name"].isin(names)]
-        else:
+        else:  # Else get the id's using the names in the df
             cur.execute(f"SELECT player_id FROM Player WHERE name in ({
                 ", ".join(["?" for _ in range(len(df))])}) ORDER BY name;",
                         df["player name"])
-
             args["player_id"] = [idx for idx, *_ in cur.fetchall()]
-
         con.close()
 
-        print([col for col in df.columns])
+        # Ensure we don't get an error when performing tSNE
+        if len(args["player_id"]) < 10:
+            abort(400, "Must include at least 10 players in the projection")
+        if len(args["col"]) < 2:
+            abort(400, "Must choose at least 2 features to project on")
+
+        # Choose the subset of columns provided
         df = df[[convert_name(c) for c in args["col"]]]
-        proj = pd.DataFrame(TSNE().fit_transform(df)).rename({
-                    0: "x", 1: "y"}, axis="columns")
+        proj = pd.DataFrame(TSNE(perplexity=min(len(args["player_id"])-1, 30))
+                            .fit_transform(df)).rename({0: "x", 1: "y"},
+                                                       axis="columns")
+        # Use player_id as the index
         proj.insert(0, "player_id", args["player_id"])
         proj.set_index("player_id", inplace=True)
-        # TODO: check this, what if perplexity < len(args[player_id])
-        print(proj.head())
+
         return proj.to_json()
-        # result = [dict(zip([col[0] for col in cur.description], row)) for row
-        #           in cur.fetchall()]
-        # return result
